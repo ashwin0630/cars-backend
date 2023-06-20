44ba8ad38eafc8223f97ef5ba7b15b3053a0a34c
ashwin0630 <ashwinkumar.sukumar@gmail.com>
2023-06-20 17:42:34 +0530

    postgres

--- cars/cars/settings.py
+++ cars/cars/settings.py
@@ -84,12 +84,15 @@ WSGI_APPLICATION = 'cars.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'railway',
+       'USER': 'postgres',
+       'PASSWORD': 'o7456D5hvAdycdkbn7nR',
+       'HOST': 'containers-us-west-66.railway.app',
+       'PORT': '6924',
+   }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
